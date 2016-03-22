a4110e48a6be4b343460c27e0c7580cb7a437a50
Darren Engwirda <d_engwirda@outlook.com>
2016-03-22 17:08:54 -0400

    Update drawmesh.m

--- drawmesh.m
+++ drawmesh.m
@@ -1,22 +1,24 @@
-function drawmesh(varargin)
+function drawmesh(mesh,varargin)
 %DRAWMESH draw mesh data for JIGSAW.
 
-%
+%---------------------------------------------------------------------
 %   Darren Engwirda
 %   github.com/dengwirda/jigsaw-matlab
-%   21-Mar-2016
+%   22-Mar-2016
 %   d_engwirda@outlook.com
+%---------------------------------------------------------------------
 %
 
-    mesh = []; opts = [];
+    opts = []; fig1 = []; fig2 = []; fig3 = [];
 
-    if (nargin >= +1), mesh = varargin{1}; end
-    if (nargin >= +2), opts = varargin{2}; end
+    if (nargin >= +2), opts = varargin{1}; end
     
-    if ( isempty(mesh))
-        error('DRAWMESH: insufficient inputs.');
+    if (nargin <= +0 || nargin >= +3)
+        error('Incorrect number of arguments!') ;
     end
-
+    
+    if (~meshhas(mesh,'point')) , return ; end
+    
     ndim = size(mesh.point.coord,2)-1 ;
  
     if (~isempty(opts) && isfield(opts,'flips'))
@@ -24,14 +26,12 @@ function drawmesh(varargin)
             mesh.point.coord(:,opts.flips) ;
     end
     
-    fig1 = []; fig2 = []; fig3 = [];
-    
     switch (ndim)
         case +2
     %-- draw mesh obj. in R^2
     
-        if (isfield(mesh,'edge2') && ~isempty(mesh.edge2))
-            
+        if (meshhas(mesh,'edge2'))
+        %-- draw EDGE2 mesh obj.
             if (isempty(fig1))
                 fig1 = figure; 
             else
@@ -42,15 +42,15 @@ function drawmesh(varargin)
             axis image off ;
             if (~isempty(opts) && ...
                     isfield(opts,'title'))
-                title([opts.title,' (EDGE-2)']);
+                title([opts.title,' (TOPO-1)']);
             end
             set(gcf,'color','w');
-            set(gca,'units','normalized','position',[.05,.05,.90,.90]);
-        
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);
         end
             
-        if (isfield(mesh,'tria3') && ~isempty(mesh.tria3))
-            
+        if (meshhas(mesh,'tria3'))
+        %-- draw TRIA3 mesh obj.
             if (isempty(fig2))
                 fig2 = figure; 
             else
@@ -61,18 +61,38 @@ function drawmesh(varargin)
             axis image off ;
             if (~isempty(opts) && ...
                     isfield(opts,'title'))
-                title([opts.title,' (TRIA-3)']);
+                title([opts.title,' (TOPO-2)']);
             end
             set(gcf,'color','w');
-            set(gca,'units','normalized','position',[.05,.05,.90,.90]);
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);
+        end
+        
+        if (meshhas(mesh,'quad4'))
+        %-- draw QUAD4 mesh obj.
+            if (isempty(fig2))
+                fig2 = figure; 
+            else
+                figure (fig2);
+            end
+            drawquad4(mesh.point.coord(:,1:2), ...
+                      mesh.quad4.index(:,1:4)) ;
+            axis image off ;
+            if (~isempty(opts) && ...
+                    isfield(opts,'title'))
+                title([opts.title,' (TOPO-2)']);
+            end
+            set(gcf,'color','w');
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);
         
         end
             
         case +3
     %-- draw mesh obj. in R^3
     
-        if (isfield(mesh,'edge2') && ~isempty(mesh.edge2))
-            
+        if (meshhas(mesh,'edge2'))
+        %-- draw EDGE2 mesh obj.
             if (isempty(fig1))
                 fig1 = figure; 
             else
@@ -87,22 +107,22 @@ function drawmesh(varargin)
             end
             if (~isempty(opts) && ...
                     isfield(opts,'title'))
-                title([opts.title,' (EDGE-2)']);
+                title([opts.title,' (TOPO-1)']);
             end
             if (~isempty(opts) && ...
                     isfield(opts,'light'))
                 if (opts.light)
-                    camlight headlight ; 
+                    camlight headlight ;
                     cameramenu;
                 end
             end
             set(gcf,'color','w');
-            set(gca,'units','normalized','position',[.05,.05,.90,.90]);
-        
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);
         end
             
-        if (isfield(mesh,'tria3') && ~isempty(mesh.tria3))
-            
+        if (meshhas(mesh,'tria3'))
+        %-- draw TRIA3 mesh obj.
             if (isempty(fig2))
                 fig2 = figure; 
             else
@@ -117,7 +137,7 @@ function drawmesh(varargin)
             end
             if (~isempty(opts) && ...
                     isfield(opts,'title'))
-                title([opts.title,' (TRIA-3)']);
+                title([opts.title,' (TOPO-2)']);
             end
             if (~isempty(opts) && ...
                     isfield(opts,'light'))
@@ -127,12 +147,12 @@ function drawmesh(varargin)
                 end
             end
             set(gcf,'color','w');
-            set(gca,'units','normalized','position',[.05,.05,.90,.90]);
-        
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);      
         end
         
-        if (isfield(mesh,'quad4') && ~isempty(mesh.quad4))
-            
+        if (meshhas(mesh,'quad4'))
+        %-- draw QUAD4 mesh obj.
             if (isempty(fig2))
                 fig2 = figure; 
             else
@@ -147,7 +167,7 @@ function drawmesh(varargin)
             end
             if (~isempty(opts) && ...
                     isfield(opts,'title'))
-                title([opts.title,' (QUAD-4)']);
+                title([opts.title,' (TOPO-2)']);
             end
             if (~isempty(opts) && ...
                     isfield(opts,'light'))
@@ -157,12 +177,12 @@ function drawmesh(varargin)
                 end
             end
             set(gcf,'color','w');
-            set(gca,'units','normalized','position',[.05,.05,.90,.90]);
-        
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);
         end
         
-        if (isfield(mesh,'tria4') && ~isempty(mesh.tria4))
-        
+        if (meshhas(mesh,'tria4'))
+        %-- draw TRIA4 mesh obj.
             if (isempty(fig3))
                 fig3 = figure; 
             else
@@ -177,7 +197,7 @@ function drawmesh(varargin)
             end
             if (~isempty(opts) && ...
                     isfield(opts,'title'))
-                title([opts.title,' (TRIA-4)']);
+                title([opts.title,' (TOPO-3)']);
             end
             if (~isempty(opts) && ...
                     isfield(opts,'light'))
@@ -187,12 +207,12 @@ function drawmesh(varargin)
                 end
             end
             set(gcf,'color','w');
-            set(gca,'units','normalized','position',[.05,.05,.90,.90]);
-        
+            set(gca,'units', ...
+                'normalized','position',[.05,.05,.90,.90]);
         end
         
         otherwise
-        error('DRAWMESH: unsupported inputs.' );
+        error('DRAWMESH: unsupported inputs.');
         
     end
     
@@ -257,32 +277,53 @@ function drawquad4(pp,q4,varargin)
     
 end
 
-function fe = surftria3(t4)
-%SURFTRIA3 return surface facets from TRIA4 mesh.
+function [fe] = surftria4(t4)
+%SURFTRIA4 return surface facets from TRIA4 mesh.
 
     ff = [t4(:,[1,2,3]);
           t4(:,[1,4,2]);
           t4(:,[2,4,3]);
           t4(:,[3,4,1])] ;
       
-    [junk,ii,jj] = unique(sort(ff,2),'rows');
+   [fj,ii,jj] = unique (sort(ff,2),'rows');
     
     ff = ff(ii,:);
     
-    ss = zeros(max(jj),1);
-    for ip = 1 : size(jj,1)
-        ss(jj(ip)) = ss(jj(ip)) + 1;
-    end
+    ss = accumarray(jj, ...
+        ones(size(jj)),[size(ff,1),1]) ;
+    
     fe = ff(ss==+1,:);  % external faces
    %fi = ff(ss~=+1,:);  % internal faces
 
 end
 
+function [fe] = surfhexa8(q8)
+%SURFHEXA8 return surface facets from HEXA8 mesh.
+
+    ff = [q8(:,[1,2,3,4]);
+          q8(:,[1,2,3,4]);
+          q8(:,[1,2,3,4]);
+          q8(:,[1,2,3,4]);
+          q8(:,[1,2,3,4]);
+          q8(:,[1,2,3,4])] ;
+
+   [fj,ii,jj] = unique (sort(ff,2),'rows');
+    
+    ff = ff(ii,:);
+    
+    ss = accumarray(jj, ...
+        ones(size(jj)),[size(ff,1),1]) ;
+    
+    fe = ff(ss==+1,:);  % external faces
+   %fi = ff(ss~=+1,:);  % internal faces
+      
+end
+
 function drawtria4(pp,t4,varargin)
-%DRAWTRIA4 draw TRIA4 elements, split based on ti
+%DRAWTRIA4 draw TRIA4 elements.
 
     if (nargin >= 3)
-    ti = varargin{1} ; 
+        ti = varargin{+1} ; 
     else
 %-- calc. default R^3 splitting plane        
         ip = unique(t4(:));
@@ -302,34 +343,39 @@ function drawtria4(pp,t4,varargin)
     ei = [.25,.25,.25];
     fe = [.95,.95,.50];
     fi = [.95,.95,.90];
-   
-    ff = surftria3(t4(: ,:));
-    fs = surftria3(t4(ti,:));
-     
-    ee = ismember(fs,ff,'rows'); % common facets
+
+    f1 = surftria4(t4( ti,:));
+    f2 = surftria4(t4(~ti,:));
     
-    patch('faces',fs( ee,:),'vertices',pp,...
+    c1 = ismember(sort(f1,2), ...
+                  sort(f2,2),'rows'); % common facets
+    c2 = ismember(sort(f2,2), ...
+                  sort(f1,2),'rows');
+    
+%-- draw external surface
+    patch('faces',f1(~c1,:),'vertices',pp,...
         'facecolor',fe,...
         'edgecolor',ec,...
         'linewidth',.40,...
+        'edgelighting','none',...
         'facelighting','flat');
-    patch('faces',fs(~ee,:),'vertices',pp,...
+%-- draw internal surface
+    patch('faces',f1( c1,:),'vertices',pp,...
         'facecolor',fi,...
         'edgecolor',ei,...
         'linewidth',.40,...
         'edgelighting','none',...
         'facelighting','flat');
-    
-    fs = surftria3(t4(~ti,:));
-    
-    ee = ismember(fs,ff,'rows'); % common facets
-    
-    patch('faces',fs( ee,:),'vertices',pp,...
+%-- draw transparent part
+    patch('faces',f2(~c2,:),'vertices',pp,...
         'facecolor',fe,...
         'edgecolor','none',...
+        'linewidth',.40,...
+        'edgelighting','none',...
         'facelighting','flat',...
-        'facealpha',+.200);
-     
+        'facealpha',.20);
+
 end
 
 
+     
