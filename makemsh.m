98102a24b970a8a8a74c21e495476bb3f33fe29c
Darren Engwirda <d_engwirda@outlook.com>
2016-03-22 17:16:40 -0400

    Update makemsh.m

--- makemsh.m
+++ makemsh.m
@@ -3,48 +3,50 @@ function makemsh(name,mesh)
 %
 %   MAKEMSH(NAME,MESH);
 %
-%   The following entities are optionally written to "NAME.MSH". Entities 
-%   are written if they are present in the sructure MESH:
+%   The following entities are optionally written to "NAME.MSH". Ent-
+%   ities are written if they are present in the sructure MESH:
 %
-%   MESH.POINT.COORD - [NPxND] array of point coordinates, where ND is the
-%       number of spatial dimenions.
+%   MESH.POINT.COORD - [NPxND] array of point coordinates, where ND 
+%       is the number of spatial dimenions.
 %
-%   MESH.EDGE2.INDEX - [N2x 3] array of indexing for edge-2 elements, whe-
-%       re INDEX(K,1:2) is the array of "points" associated with the K-TH 
-%       edge, and INDEX(K,3) is an ID tag for the K-TH edge.
+%   MESH.EDGE2.INDEX - [N2x 3] array of indexing for edge-2 elements, 
+%       where INDEX(K,1:2) is an array of "points" associated with 
+%       the K-TH edge, and INDEX(K,3) is an ID tag for the K-TH edge.
 %
-%   MESH.TRIA3.INDEX - [N3x 4] array of indexing for tria-3 elements, whe-
-%       re INDEX(K,1:3) is the array of "points" associated with the K-TH 
-%       tria, and INDEX(K,4) is an ID tag for the K-TH tria.
+%   MESH.TRIA3.INDEX - [N3x 4] array of indexing for tria-3 elements, 
+%       where INDEX(K,1:3) is an array of "points" associated with 
+%       the K-TH tria, and INDEX(K,4) is an ID tag for the K-TH tria.
 %
-%   MESH.QUAD4.INDEX - [N4x 5] array of indexing for quad-4 elements, whe-
-%       re INDEX(K,1:4) is the array of "points" associated with the K-TH 
-%       quad, and INDEX(K,5) is an ID tag for the K-TH quad.
+%   MESH.QUAD4.INDEX - [N4x 5] array of indexing for quad-4 elements, 
+%       where INDEX(K,1:4) is an array of "points" associated with 
+%       the K-TH quad, and INDEX(K,5) is an ID tag for the K-TH quad.
 %
-%   MESH.TRIA4.INDEX - [M4x 5] array of indexing for tria-4 elements, whe-
-%       re INDEX(K,1:4) is the array of "points" associated with the K-TH 
-%       tria, and INDEX(K,5) is an ID tag for the K-TH tria.
+%   MESH.TRIA4.INDEX - [M4x 5] array of indexing for tria-4 elements, 
+%       where INDEX(K,1:4) is an array of "points" associated with 
+%       the K-TH tria, and INDEX(K,5) is an ID tag for the K-TH tria.
 %
-%   MESH.HEXA8.INDEX - [M8x 9] array of indexing for hexa-8 elements, whe-
-%       re INDEX(K,1:8) is the array of "points" associated with the K-TH 
-%       hexa, and INDEX(K,9) is an ID tag for the K-TH hexa.
+%   MESH.HEXA8.INDEX - [M8x 9] array of indexing for hexa-8 elements, 
+%       where INDEX(K,1:8) is an array of "points" associated with 
+%       the K-TH hexa, and INDEX(K,9) is an ID tag for the K-TH hexa.
 %
-%   MESH.WEDG6.INDEX - [M6x 7] array of indexing for wedg-6 elements, whe-
-%       re INDEX(K,1:6) is the array of "points" associated with the K-TH 
-%       wedg, and INDEX(K,7) is an ID tag for the K-TH wedg.
+%   MESH.WEDG6.INDEX - [M6x 7] array of indexing for wedg-6 elements, 
+%       where INDEX(K,1:6) is an array of "points" associated with 
+%       the K-TH  wedg, and INDEX(K,7) is an ID tag for the K-TH wedg.
 %
-%   MESH.PYRA5.INDEX - [M5x 6] array of indexing for pyra-5 elements, whe-
-%       re INDEX(K,1:5) is the array of "points" associated with the K-TH 
-%       pyra, and INDEX(K,6) is an ID tag for the K-TH pyra.
+%   MESH.PYRA5.INDEX - [M5x 6] array of indexing for pyra-5 elements, 
+%       where INDEX(K,1:5) is an array of "points" associated with 
+%       the K-TH pyra, and INDEX(K,6) is an ID tag for the K-TH pyra.
 %
 %   See also READMSH, MAKEVTK, READVTK, MAKEMESH, READMESH, READOFF, 
 %            MAKEOFF
-
 %
+
+%---------------------------------------------------------------------
 %   Darren Engwirda
 %   github.com/dengwirda/jigsaw-matlab
-%   20-Mar-2016
+%   22-Mar-2016
 %   d_engwirda@outlook.com
+%---------------------------------------------------------------------
 %
 
     if (~ischar  (name))
@@ -67,124 +69,103 @@ function makemsh(name,mesh)
     
     nver = +1;
     
-    endl = '\n' ;
-    real = '%1.16g;';
-    ints = '%i;';
+    fprintf(ffid,['# %s.msh file, created by JIGSAW','\n'],file);
+    fprintf(ffid,['mshid=%u','\n'],nver);
     
-    fprintf(ffid,['# %s.msh file, created by JIGSAW',endl],file);
-    fprintf(ffid,['mshid=%u',endl],nver);
-    
-    if (isfield(mesh,'point') && ...
-        isfield(mesh.point,'coord') && ...
-       ~isempty(mesh.point.coord) )
-    
-    ndim = size(mesh.point.coord,2) - 1 ;
-    
-    fprintf(ffid,['ndims=%u',endl],ndim);
+    if (meshhas(mesh,'point'))
     
 %-- write "POINT" data
         
-    fprintf(ffid,['point=%u',endl],size(mesh.point.coord,1));        
-    fprintf(ffid,[repmat(real,1,ndim),'%i',endl],...
+    ndim = size(mesh.point.coord,2) - 1 ;    
+    fprintf(ffid,['ndims=%u','\n'],ndim);
+        
+    fprintf(ffid,['point=%u','\n'],size(mesh.point.coord,1));        
+    fprintf(ffid,[repmat('%1.16g;',1,ndim),'%i','\n'],...
         mesh.point.coord' ) ;
 
     end
     
-    if (isfield(mesh,'edge2') && ...
-        isfield(mesh.edge2,'index') && ...
-       ~isempty(mesh.edge2.index) )
-       
+    if (meshhas(mesh,'edge2'))
+        
 %-- write "EDGE2" data
         
     index = mesh.edge2.index;
     index(:,1:2) = index(:,1:2)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['edge2=%u',endl],size(mesh.edge2.index,1));        
-    fprintf(ffid,[repmat(ints,1,2),'%i',endl],index');
+    fprintf(ffid,['edge2=%u','\n'],size(mesh.edge2.index,1));        
+    fprintf(ffid,[repmat('%u;',1,2),'%i','\n'],index');
 
     end
     
-    if (isfield(mesh,'tria3') && ...
-        isfield(mesh.tria3,'index') && ...
-       ~isempty(mesh.tria3.index) )
+    if (meshhas(mesh,'tria3'))
        
 %-- write "TRIA3" data
         
     index = mesh.tria3.index;
     index(:,1:3) = index(:,1:3)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['tria3=%u',endl],size(mesh.tria3.index,1));        
-    fprintf(ffid,[repmat(ints,1,3),'%i',endl],index');
+    fprintf(ffid,['tria3=%u','\n'],size(mesh.tria3.index,1));        
+    fprintf(ffid,[repmat('%u;',1,3),'%i','\n'],index');
 
     end
     
-    if (isfield(mesh,'quad4') && ...
-        isfield(mesh.quad4,'index') && ...
-       ~isempty(mesh.quad4.index) )
+    if (meshhas(mesh,'quad4'))
        
 %-- write "QUAD4" data
         
     index = mesh.quad4.index;
     index(:,1:4) = index(:,1:4)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['quad4=%u',endl],size(mesh.quad4.index,1));        
-    fprintf(ffid,[repmat(ints,1,4),'%i',endl],index');
+    fprintf(ffid,['quad4=%u','\n'],size(mesh.quad4.index,1));        
+    fprintf(ffid,[repmat('%u;',1,4),'%i','\n'],index');
 
     end
     
-    if (isfield(mesh,'tria4') && ...
-        isfield(mesh.tria4,'index') && ...
-       ~isempty(mesh.tria4.index) )
+    if (meshhas(mesh,'tria4'))
 
 %-- write "TRIA4" data
         
     index = mesh.tria4.index;
     index(:,1:4) = index(:,1:4)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['tria4=%u',endl],size(mesh.tria4.index,1));        
-    fprintf(ffid,[repmat(ints,1,4),'%i',endl],index');
+    fprintf(ffid,['tria4=%u','\n'],size(mesh.tria4.index,1));        
+    fprintf(ffid,[repmat('%u;',1,4),'%i','\n'],index');
 
     end
     
-    if (isfield(mesh,'hexa8') && ...
-        isfield(mesh.hexa8,'index') && ...
-       ~isempty(mesh.hexa8.index) )
+    if (meshhas(mesh,'hexa8'))
     
 %-- write "HEXA8" data
         
     index = mesh.hexa8.index;
     index(:,1:8) = index(:,1:8)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['hexa8=%u',endl],size(mesh.hexa8.index,1));        
-    fprintf(ffid,[repmat(ints,1,8),'%i',endl],index');
+    fprintf(ffid,['hexa8=%u','\n'],size(mesh.hexa8.index,1));        
+    fprintf(ffid,[repmat('%u;',1,8),'%i','\n'],index');
 
     end
     
-    if (isfield(mesh,'wedg6') && ...
-        isfield(mesh.wedg6,'index') && ...
-       ~isempty(mesh.wedg6.index) )
+    if (meshhas(mesh,'wedg6'))
     
 %-- write "WEDG6" data
         
     index = mesh.wedg6.index;
     index(:,1:6) = index(:,1:6)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['wedg6=%u',endl],size(mesh.wedg6.index,1));        
-    fprintf(ffid,[repmat(ints,1,6),'%i',endl],index');
+    fprintf(ffid,['wedg6=%u','\n'],size(mesh.wedg6.index,1));        
+    fprintf(ffid,[repmat('%u;',1,6),'%i','\n'],index');
 
     end
     
-    if (isfield(mesh,'pyra5') && ...
-        isfield(mesh.pyra5,'index') && ...
-       ~isempty(mesh.ptra5.index) )
+    if (meshhas(mesh,'pyra5'))
     
 %-- write "PYRA5" data
         
     index = mesh.pyra5.index;
     index(:,1:5) = index(:,1:5)-1 ; % file is zero-indexed!
        
-    fprintf(ffid,['pyra5=%u',endl],size(mesh.pyra5.index,1));        
-    fprintf(ffid,[repmat(ints,1,6),'%i',endl],index');
+    fprintf(ffid,['pyra5=%u','\n'],size(mesh.pyra5.index,1));        
+    fprintf(ffid,[repmat('%u;',1,6),'%i','\n'],index');
 
     end
         
